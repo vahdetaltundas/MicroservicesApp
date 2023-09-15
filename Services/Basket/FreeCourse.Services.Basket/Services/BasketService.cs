2c458e2973f856c6fc5bb564f5ec4e53e082269d
Vahdet Altundaş <vahdetaltundas2323@gmail.com>
2023-09-15 21:05:16 +0300

    Basket Service Impl

---
+++ Services/Basket/FreeCourse.Services.Basket/Services/BasketService.cs
@@ -0,0 +1,40 @@
+﻿using FreeCourse.Services.Basket.Dtos;
+using FreeCourse.Shared.Dtos;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace FreeCourse.Services.Basket.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly RedisService _redisService;
+
+        public BasketService(RedisService redisService)
+        {
+            _redisService = redisService;
+        }
+
+        public async Task<Response<bool>> Delete(string userId)
+        {
+            var status= await _redisService.GetDb().KeyDeleteAsync(userId);
+            return status ? Response<bool>.Success(204) : Response<bool>.Fail("Sepet bulunamadı", 404);
+        }
+
+        public async Task<Response<BasketDto>> GetBasket(string userId)
+        {
+            var existBasket = await _redisService.GetDb().StringGetAsync(userId);
+            if(String.IsNullOrEmpty(existBasket))
+            {
+                return Response<BasketDto>.Fail("Sepet Bulunamadı", 404);
+            }
+            return Response<BasketDto>.Success(JsonSerializer.Deserialize<BasketDto>(existBasket), 200);
+
+        }
+
+        public async Task<Response<bool>> SaveOrUpdate(BasketDto basketDto)
+        {
+            var status = await _redisService.GetDb().StringSetAsync(basketDto.UserId,JsonSerializer.Serialize(basketDto));
+            return status ? Response<bool>.Success(204) : Response<bool>.Fail("Sepet kayıt yada güncelleme yapamadı", 500);
+        }
+    }
+}
