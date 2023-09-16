dadf69d14fe64f5648e35fd8515a2425a663c0cb
Vahdet Altundaş <vahdetaltundas2323@gmail.com>
2023-09-16 15:59:43 +0300

    Basket Microservice Impl

---
+++ Services/Basket/FreeCourse.Services.Basket/Controllers/BasketsController.cs
@@ -0,0 +1,41 @@
+﻿using FreeCourse.Services.Basket.Dtos;
+using FreeCourse.Services.Basket.Services;
+using FreeCourse.Shared.ControllerBases;
+using FreeCourse.Shared.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.Basket.Controllers
+{
+    public class BasketsController : CustomBaseController
+    {
+        private readonly IBasketService _basketService;
+        private readonly ISharedIdentityService _sharedIdentityService;
+        public BasketsController(IBasketService basketService, ISharedIdentityService sharedIdentityService)
+        {
+            _basketService = basketService;
+            _sharedIdentityService = sharedIdentityService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBasket()
+        {
+            var claims = User.Claims;
+            return CreateActionResult(await _basketService.GetBasket(_sharedIdentityService.GetUserId));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveOrUpdateBasket(BasketDto basketDto)
+        {
+            var response = await _basketService.SaveOrUpdate(basketDto);
+            return CreateActionResult(response);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteBasket()
+        {
+            return CreateActionResult(await _basketService.Delete(_sharedIdentityService.GetUserId));
+        }
+
+    }
+}
--- Services/Basket/FreeCourse.Services.Basket/Program.cs
+++ Services/Basket/FreeCourse.Services.Basket/Program.cs
@@ -1,10 +1,26 @@
 using FreeCourse.Services.Basket.Services;
 using FreeCourse.Services.Basket.Settings;
+using FreeCourse.Shared.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Options;
+using System.IdentityModel.Tokens.Jwt;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var requreAuthorizePolicy= new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
 // Add services to the container.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = builder.Configuration["IdentityServerURL"];
+    options.Audience = "resource_basket";
+    options.RequireHttpsMetadata = false;   
+});
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<ISharedIdentityService,SharedIdentityService>();
+builder.Services.AddScoped<IBasketService,BasketService>();
 builder.Services.Configure<RedisSettings>(builder.Configuration.GetSection("RedisSettings"));
 builder.Services.AddSingleton<RedisService>(sp =>
 {
@@ -13,7 +29,10 @@ builder.Services.AddSingleton<RedisService>(sp =>
     redis.Connect();
     return redis;
 });
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add(new AuthorizeFilter(requreAuthorizePolicy));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -27,6 +46,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
